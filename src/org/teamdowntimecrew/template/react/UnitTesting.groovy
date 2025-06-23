470e6656c7a60d79cd44394389ab18a82299fe82
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-23 15:34:11 +0530

    Update UnitTesting.groovy

--- src/org/teamdowntimecrew/template/react/UnitTesting.groovy
+++ src/org/teamdowntimecrew/template/react/UnitTesting.groovy
@@ -1,1 +1,23 @@
+package org.teamdowntimecrew.frontend
 
+class UnitTesting implements Serializable {
+    def steps
+
+    UnitTesting(steps) {
+        this.steps = steps
+    }
+
+    def run() {
+            try {
+                steps.sh '''
+                    mkdir -p test-reports
+                    npm test
+                    echo "Generated reports:"
+                    ls -la test-reports/
+                '''
+            } finally {
+                steps.archiveArtifacts artifacts: 'test-reports/**/*', allowEmptyArchive: true
+            }
+        }
+    }
+}
