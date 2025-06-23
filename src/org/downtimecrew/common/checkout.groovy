3cf486e1633ce4330ee1cf20b9895cc8dd7f5357
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-23 10:34:05 +0530

    Update checkout.groovy

--- src/org/downtimecrew/common/checkout.groovy
+++ src/org/downtimecrew/common/checkout.groovy
@@ -1,1 +1,19 @@
+package org.teamdowntimecrew.common
 
+class GitClone {
+    
+    static void cloneRepo(context, Map config = [:]) {
+        def repoName = config.repoName ?: error("Missing repoName")
+        def branch = config.branch ?: 'main'
+        def url = config.url ?: error("Missing Git repo URL")
+        def credentialsId = config.credentialsId ?: error("Missing credentialsId")
+
+        context.dir(repoName) {
+            context.git branch: branch, credentialsId: credentialsId, url: url
+        }
+    }
+
+    private static void error(String message) {
+        throw new IllegalArgumentException(message)
+    }
+}
