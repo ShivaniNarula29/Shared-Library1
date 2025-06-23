e4785c3affbd89281899de4e53cf31bc2c0ffc0f
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-23 10:24:01 +0530

    Update and rename unittesting.groovy to unittesting.groovy

---
+++ src/org/downtimecrew/template/go/unittesting.groovy
@@ -0,0 +1,50 @@
+
+package org.downtimecrew.application.golang.unit_testing
+
+class GoUnitTesting implements Serializable {
+    def steps
+
+    GoUnitTesting(steps) {
+        this.steps = steps
+    }
+
+    def installGoJunitReport() {
+        steps.stage('Install go-junit-report') {
+            steps.sh 'go install github.com/jstemmer/go-junit-report@latest'
+        }
+    }
+
+    def runTestsAndGenerateReports() {
+        steps.stage('Run Tests and Generate Reports') {
+            steps.sh '''
+                export PATH=$HOME/go/bin:$PATH
+                mkdir -p test-reports coverage-reports
+                go test -v -coverprofile=coverage-reports/coverage.out ./... | go-junit-report > test-reports/report.xml
+                go tool cover -html=coverage-reports/coverage.out -o coverage-reports/coverage.html
+            '''
+        }
+    }
+
+    def publishJUnitReport() {
+        steps.stage('Publish JUnit Report') {
+            steps.junit 'test-reports/report.xml'
+        }
+    }
+
+    def publishCoverageReport() {
+        steps.stage('Publish Report') {
+            steps.publishHTML(target: [
+                reportDir: 'coverage-reports',
+                reportFiles: 'coverage.html',
+                reportName: 'Go Report',
+                keepAll: true
+            ])
+        }
+    }
+
+    def archiveAndCleanup() {
+        steps.stage('Cleanup') {
+            steps.echo "Cleaning up workspace"
+        }
+    }
+}
