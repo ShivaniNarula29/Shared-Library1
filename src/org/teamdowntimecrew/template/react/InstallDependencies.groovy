8db2c1ae0120f8ecf3c54a5fee46b4c933939c8c
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-23 15:18:57 +0530

    Create InstallDependencies.groovy

---
+++ src/org/teamdowntimecrew/template/react/InstallDependencies.groovy
@@ -0,0 +1,25 @@
+package org.teamdowntimecrew.template.react
+
+class InstallDependencies implements Serializable {
+    def steps
+
+    InstallDependencies(steps) {
+        this.steps = steps
+    }
+
+    def install() {
+        steps.stage('Install Dependencies') {
+            steps.sh '''
+                npm install --save-dev \\
+                jest@26.6.3 \\
+                babel-jest@26.6.3 \\
+                @testing-library/react@12.1.2 \\
+                @testing-library/jest-dom@5.16.5 \\
+                @testing-library/user-event@13.5.0 \\
+                jest-environment-jsdom@26.6.2 \\
+                react-test-renderer@16.14.0 \\
+                --legacy-peer-deps
+            '''
+        }
+    }
+}
