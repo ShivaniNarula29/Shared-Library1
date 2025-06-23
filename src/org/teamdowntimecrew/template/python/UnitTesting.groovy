fa3929a0aa8c9bdedd9dbf3a7e9142013a302348
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-24 00:49:36 +0530

    Update UnitTesting.groovy

--- src/org/teamdowntimecrew/template/python/UnitTesting.groovy
+++ src/org/teamdowntimecrew/template/python/UnitTesting.groovy
@@ -1,1 +1,31 @@
 
+package org.teamdowntimecrew.template.python
+
+class UnitTesting implements Serializable {
+    def steps
+
+    UnitTesting(steps) {
+        this.steps = steps
+    }
+
+    void runTestsAndGenerateReports(Map args = [:]) {
+        def reportFile = args.get('reportFile', 'unit_test_report.txt')
+        def venvDir = 'venv'
+            steps.echo "🔧 Setting up Python virtual environment and running unit tests..."
+
+            steps.sh """
+                set -e
+                python3 -m venv ${venvDir}
+                . ${venvDir}/bin/activate
+                pip install --upgrade pip
+                if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+                pip install pytest
+                pytest > ${reportFile} || true
+                deactivate
+            """
+
+            // Archive report
+            steps.archiveArtifacts artifacts: reportFile, allowEmptyArchive: true
+       
+    }
+}
