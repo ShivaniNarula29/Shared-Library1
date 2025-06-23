2fde8fb5b1a5604e20c050f2be9b006d77c3e412
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-23 14:46:26 +0530

    Update UnitTesting.groovy

--- src/org/teamdowntimecrew/template/go/UnitTesting.groovy
+++ src/org/teamdowntimecrew/template/go/UnitTesting.groovy
@@ -8,12 +8,10 @@ class UnitTesting implements Serializable {
     }
 
     def runTestsAndGenerateReports() {
-        steps.stage('Run Tests and Generate Reports') {
             steps.sh '''
                 mkdir -p test-reports
                 go test ./... -run=^$ -skip=./api -v > test-reports/test-output.txt || true
             '''
             steps.archiveArtifacts artifacts: 'test-reports/test-output.txt', fingerprint: true
-        }
     }
 }
