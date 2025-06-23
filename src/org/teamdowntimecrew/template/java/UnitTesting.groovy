f0d9dc10eecbb61cd14e20d8d77caf0622625752
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-23 16:54:54 +0530

    Update UnitTesting.groovy

--- src/org/teamdowntimecrew/template/java/UnitTesting.groovy
+++ src/org/teamdowntimecrew/template/java/UnitTesting.groovy
@@ -1,1 +1,17 @@
+package org.teamdowntimecrew.template.java
 
+class UnitTesting implements Serializable {
+    def steps
+
+    UnitTesting(steps) {
+        this.steps = steps
+    }
+
+    def runTestsAndGenerateReports() {
+        steps.stage('Run Java Unit Tests') {
+            steps.sh 'mvn clean test'
+            steps.junit '**/target/surefire-reports/*.xml'
+            steps.archiveArtifacts artifacts: 'target/surefire-reports/**/*.*', fingerprint: true
+        }
+    }
+}
