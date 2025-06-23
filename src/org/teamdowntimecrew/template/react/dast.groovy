b14aa70737ff071d506880cdd3ebb10c026d57da
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-06-23 18:47:07 +0530

    Update dast.groovy

--- src/org/teamdowntimecrew/template/react/dast.groovy
+++ src/org/teamdowntimecrew/template/react/dast.groovy
@@ -1,1 +1,18 @@
+package org.teamdowntimecrew.template.react
 
+class Dast implements Serializable {
+    def script
+
+    Dast(script) {
+        this.script = script
+    }
+
+    def call(Map config = [:]) {
+        if (!config.TARGET_URL || !config.ZAP_HOME || !config.ZAP_DIR || !config.ZAP_PORT) {
+            throw new IllegalArgumentException("Missing required ZAP configuration: TARGET_URL, ZAP_HOME, ZAP_DIR, ZAP_PORT")
+        }
+
+        def zapRunner = new owaspzap(script)
+        zapRunner.call(config)
+    }
+}
